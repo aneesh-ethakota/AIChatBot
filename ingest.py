e679bc23601426fc472a5fadb47eba2fcac1cd29
aneesh-ethakota <149597153+aneesh-ethakota@users.noreply.github.com>
2023-11-01 20:34:23 -0400

    program to load the document for the company

--- ingest.py
+++ ingest.py
@@ -1,1 +1,33 @@
-#generate the vectore store
+"""Load documents for the company."""
+import os
+import pickle
+
+from langchain.document_loaders import DirectoryLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+
+#key is needed to connect to chatgpt
+os.environ["OPENAI_API_KEY"] = ''
+
+
+def ingest_docs():
+    """Get documents from web pages."""
+    loader = DirectoryLoader("C:\\knowledge_base", glob="./*")
+    raw_documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200,
+    )
+    documents = text_splitter.split_documents(raw_documents)
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_documents(documents, embeddings)
+
+    # Save vectorstore
+    with open("vectorstore_all.pkl", "wb") as f:
+        pickle.dump(vectorstore, f)
+
+
+if __name__ == "__main__":
+    ingest_docs()
+
